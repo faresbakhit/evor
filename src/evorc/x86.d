711ecc41a8e8f8136b02fa1f6e2ff1b5dc4e72ec
Fares Ahmed Bakhit <faresa.bakhit@gmail.com>
2025-07-23 23:07:26 +0300

    umm, something with codegen

--- src/evorc/x86.d
+++ src/evorc/x86.d
@@ -38,10 +38,6 @@ void x86(Writer)(scope ref Writer sink, LinFunc func)
     if (func.params.length > 3) sink.mov(mem(Reg.rbp, -16), Reg.ecx);
 
     foreach (stmt; func.block) (*stmt).match!(partial!(x86, sink));
-    
-    // function epilogue
-    sink.leave();
-    sink.ret();
 }
 
 void x86(Writer)(scope ref Writer sink, LinIf if_)
@@ -50,10 +46,132 @@ void x86(Writer)(scope ref Writer sink, LinIf if_)
 
 void x86(Writer)(scope ref Writer sink, LinAssign assign)
 {
+    assign.rhs.match!(
+        (Atom atom)
+        {
+            atom.match!(
+                (LinBool bool_)
+                {
+                    assign.lhs.match!(
+                        (LinVar var)
+                        {
+                            sink.mov(var, bool_);
+                        },
+                        (Deref deref)
+                        {
+                        },
+                    );
+                },
+                (LinInt int_)
+                {
+                    assign.lhs.match!(
+                        (LinVar var)
+                        {
+                            sink.mov(var, int_);
+                        },
+                        (Deref deref)
+                        {
+                        },
+                    );
+                },
+                (LinVar var)
+                {
+                    assign.lhs.match!(
+                        (LinVar assigneeVar)
+                        {
+                            sink.mov(var, assigneeVar);
+                        },
+                        (Deref deref)
+                        {
+                        },
+                    );
+                },
+            );
+        },
+        (LinUn un)
+        {
+        },
+        (LinBin bin)
+        {
+            with (BinOp) final switch (bin.op)
+            {
+            case add:
+                break;
+            case sub:
+                break;
+            case mul:
+                break;
+            case div:
+                break;
+            case rem:
+                break;
+            case bitwiseAnd:
+                break;
+            case bitwiseOr:
+                break;
+            case bitwiseXor:
+                break;
+            case bitwiseLeftShift:
+                break;
+            case bitwiseRightShift:
+                break;
+            case logicalAnd:
+                break;
+            case logicalOr:
+                break;
+            case equalTo:
+                break;
+            case notEqualTo:
+                break;
+            case lessThan:
+                break;
+            case greaterThan:
+                break;
+            case lessThanOrEqualTo:
+                break;
+            case greaterThanOrEqualTo:
+                break;
+            case arraySubscript:
+                break;
+            case memberAccess:
+                break;
+            case memberAccessThroughPointer:
+                break;
+            }
+        },
+        (LinCall call)
+        {
+        },
+    );
 }
 
 void x86(Writer)(scope ref Writer sink, LinReturn ret)
 {
+    if (ret.expr.isNull)
+    {
+        // function epilogue
+        sink.leave();
+        sink.ret();
+        return;
+    }
+    auto expr = ret.expr.get;
+    ret.expr.get.match!(
+        (Atom atom)
+        {
+            atom.match!(a => sink.mov(Reg.eax, a));
+        },
+        (LinUn un)
+        {
+        },
+        (LinBin bin)
+        {
+        },
+        (LinCall call)
+        {
+        },
+    );
+    sink.leave();
+    sink.ret();
 }
 
 void x86(Writer)(scope ref Writer sink, LinExpr expr)
@@ -107,13 +225,45 @@ uint nextMultipleOf16(uint n)
 
 void sub(Writer)(scope ref Writer sink, Reg reg, uint n)
 {
-    sink.formattedWrite("\tsub\t%s, %s\n", reg.toString(), n);
+    sink.formattedWrite("\tsub\t%s, %s\n", reg.x86(), n);
 }
 
 void mov(Writer, U, V)(scope ref Writer sink, U src, V dest)
 if (!(is(U : Mem) && is(V : Mem)))
 {
-    sink.formattedWrite("\tmov\t%s, %s\n", src.toString(), dest.toString());
+    sink.formattedWrite("\tmov\t%s, %s\n", src.x86(), dest.x86());
+}
+
+string x86(LinVar var)
+{
+    return (*var.type).match!(
+        (PrimitiveType type)
+        {
+            with (PrimitiveType) final switch (type)
+            {
+            case int_:
+                return "dword ptr %s".format(mem(Reg.rbp, -4 * (var.id + 1)).x86());
+            case bool_:
+                return "byte ptr %s".format(mem(Reg.rbp, -4 * (var.id + 1)).x86());
+            case void_:
+                return "NOTIMPLEMENTED";
+            }
+        },
+        (LinPointer pointer)
+        {
+            return "NOTIMPLEMENTED";
+        }
+    );
+}
+
+string x86(LinInt int_)
+{
+    return "%s".format(int_.value);
+}
+
+string x86(LinBool bool_)
+{
+    return "%s".format(cast(int)bool_.value);
 }
 
 struct Mem
@@ -123,9 +273,9 @@ struct Mem
     uint scale;
     int displacement;
 
-    string toString()
+    string x86()
     {
-        string basePart = base.toString();
+        string basePart = base.x86();
         string indexPart = "";
         string dispPart = "";
 
@@ -133,11 +283,11 @@ struct Mem
         {
             if (scale == 1)
             {
-                indexPart = format("+%s", index.get.toString(), scale);
+                indexPart = format("+%s", index.get.x86(), scale);
             }
             else
             {
-                indexPart = format("+%s*%s", index.get.toString(), scale);
+                indexPart = format("+%s*%s", index.get.x86(), scale);
             }
         }
 
@@ -162,7 +312,7 @@ Mem mem(Reg base, int displacement)
 
 void push(Writer)(scope ref Writer sink, Reg reg)
 {
-    sink.formattedWrite("\tpush\t%s\n", reg.toString());
+    sink.formattedWrite("\tpush\t%s\n", reg.x86());
 }
 
 void label(Writer)(scope ref Writer sink, string name)
@@ -172,12 +322,12 @@ void label(Writer)(scope ref Writer sink, string name)
 
 void intel_syntax(Writer)(scope ref Writer sink)
 {
-    sink.formattedWrite("\t.intel-syntax\n");
+    sink.formattedWrite("\t.intel_syntax noprefix\n");
 }
 
 void type(Writer)(scope ref Writer sink, string name, TypeDescription typeDescription)
 {
-    sink.formattedWrite("\t.type\t%s, @%s\n", name, typeDescription.toString());
+    sink.formattedWrite("\t.type\t%s, @%s\n", name, typeDescription.x86());
 }
 
 void global(Writer)(scope ref Writer sink, string symbol)
@@ -192,7 +342,7 @@ void text(Writer)(scope ref Writer sink)
 
 void section(Writer)(scope ref Writer sink, string name, BitFlags!Flag flags, Type type)
 {
-    sink.formattedWrite("\t.section\t%s,\"%s\",@%s\n", name, flags.toString(), type.toString());
+    sink.formattedWrite("\t.section\t%s,\"%s\",@%s\n", name, flags.x86(), type.x86());
 }
 
 enum TypeDescription
@@ -200,7 +350,7 @@ enum TypeDescription
     function_,
 }
 
-string toString(TypeDescription typeDescription)
+string x86(TypeDescription typeDescription)
 {
     with (TypeDescription) final switch (typeDescription)
     {
@@ -228,7 +378,7 @@ enum Reg
     esp,
 }
 
-string toString(Reg reg)
+string x86(Reg reg)
 {
     return to!string(reg);
 }
@@ -240,7 +390,7 @@ enum Flag
     executable = 1 << 1,
 }
 
-string toString(BitFlags!Flag flags)
+string x86(BitFlags!Flag flags)
 {
     if (!flags) return "";
 
@@ -256,7 +406,7 @@ enum Type
     nobits,
 }
 
-string toString(Type type)
+string x86(Type type)
 {
     return to!string(type);
 }
